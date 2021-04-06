9756eb17160c01ebb9f736d53c4eeda006b73190
Greg Hosking <ghosking@mail.endicott.edu>
2021-04-06 17:47:21 -0400

    Removed empty create_app documentation and resized main div.

--- website/__init__.py
+++ website/__init__.py
@@ -2,14 +2,10 @@ from flask import Flask
 
 
 def create_app():
-    """
-    TODO: write function documentation
-    """
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Secret Key'
 
     from .views import views
-
     app.register_blueprint(views, url_prefix='/')
 
     return app
